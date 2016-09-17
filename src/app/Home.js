f6b2736a17f4195836312d6adc9ee57032d99891
iplus <ivan.plyusnin@gmail.com>
2016-09-18 02:08:30 +0300

    Add missing component

---
+++ src/app/Home.js
@@ -0,0 +1,16 @@
+/**
+ * In this file, we create a React component
+ * which incorporates components provided by Material-UI.
+ */
+import React, {Component} from 'react';
+
+
+class Home extends Component {
+  render() {
+    return (
+            <h1>Home!</h1>
+    );
+  }
+}
+
+export default Home;
