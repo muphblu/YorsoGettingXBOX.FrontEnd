126343b35549efb0d3dae0ab9e0dcfa2fdbf0bdf
iplus <ivan.plyusnin@gmail.com>
2016-09-18 11:39:16 +0300

    bums

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -21,12 +21,12 @@ const config = {
       }
     }),
     // Minify the bundle
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        // suppresses warnings, usually from module minification
-        warnings: false,
-      },
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     // suppresses warnings, usually from module minification
+    //     warnings: false,
+    //   },
+    // }),
     // Allows error warnings but does not stop compiling.
     new webpack.NoErrorsPlugin(),
     // Transfer Files
